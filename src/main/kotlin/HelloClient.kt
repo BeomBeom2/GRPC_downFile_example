65af13924bf877f081b51df43a5a5891f3f56fde
beombeom2 <whwjdqja20@naver.com>
2021-04-06 14:22:58 +0900

    Initial commit

---
+++ src/main/kotlin/HelloClient.kt
@@ -0,0 +1,130 @@
+import com.google.protobuf.ByteString.copyFrom
+import com.main.file_proto.file_proto
+import com.main.file_proto.FileServiceGrpc
+import io.grpc.ManagedChannelBuilder
+import io.grpc.stub.StreamObserver
+import java.io.*
+import java.lang.Thread.sleep
+
+fun main() {
+    println("[GRPC, Client] main()")
+    val channel = ManagedChannelBuilder
+        .forAddress("localhost", 10004)
+        .usePlaintext()
+        .build()
+
+    val stub = FileServiceGrpc.newBlockingStub(channel)
+    val response = stub.sayHello(getHelloRequest("hello, server"))
+    sleep(3000);
+    println("[GRPC, Client] response(${response.reply})")
+
+    println("\n###########################################################")
+    println("###########################################################\n")
+    println("[GRPC, Client] file down")
+
+    val BUF_SIZE = 4096;
+    var buffer = ByteArray(BUF_SIZE)
+    var size : Int = 0
+    val output : OutputStream = BufferedOutputStream(FileOutputStream("C:\\Client\\zlib-1.2.3.exe"))
+
+    println("[GRPC, Server] file down ...")
+
+    val response1 = stub.fileDown(getHelloRequest("Client file request"))
+
+    response1.forEach {
+
+        buffer = it!!.fileBin.toByteArray()
+        size = it!!.fileSize
+        println("get file size is - ${size}")
+
+        output.write(buffer,  0, size )
+    }
+    output.close();
+
+    println("\n###########################################################")
+    println("###########################################################\n")
+
+    println("[GRPC, Client] get Server Version Info")
+
+    val asyncStub = FileServiceGrpc.newStub(channel)
+
+    val response_1 = stub.serverVersionInfo(file_proto.ver_Request.newBuilder()
+        .setVersion("v1.0.0.1")
+        .build())
+
+    response_1.forEach {
+        println("[GRPC, Client] response: ${it.version}")
+        println("[GRPC, Client] response: ${it.fileInfo}")
+        println("[GRPC, Client] response: ${it.hash}")
+    }
+
+    println("\n###########################################################")
+    println("###########################################################\n")
+
+    println("[GRPC, Client] Client Version Info")
+
+    val requestObserver_1 = asyncStub.clientVersionInfo(VersionResponseStreamObserver())
+
+    for(i in 0 until 2) {
+        requestObserver_1.onNext(file_proto.ver_Request.newBuilder()
+            .setVersion("vi.0.0.$i")
+            .setFileInfo("version explain - $i")
+            .setFileInfo("hash - ${i*3}")
+            .build())
+        sleep(100)
+    }
+    requestObserver_1.onCompleted()
+
+    println("\n###########################################################")
+    println("###########################################################\n")
+
+    println("[GRPC, Client] Client Stream to Stream ")
+
+    val requestObserver_2 = asyncStub.reqRes(HelloResponseStreamObserver())
+
+    for(i in 0 until 3) {
+        requestObserver_2.onNext(file_proto.HelloRequest.newBuilder()
+            .setGreeting("\n ${i}th Client send")
+            .build())
+        sleep(100)
+    }
+    requestObserver_2.onCompleted()
+
+    channel.shutdown()
+    println("[GRPC, Client] channel is shutdown")
+}
+
+fun getHelloRequest(greeting: String): file_proto.HelloRequest {
+    return file_proto.HelloRequest.newBuilder()
+        .setGreeting(greeting)
+        .build()
+}
+
+class HelloResponseStreamObserver : StreamObserver<file_proto.HelloResponse> {
+    override fun onNext(value: file_proto.HelloResponse?) {
+        for(i in 0 until 2) {
+            println("[GRPC, Client] send version = ${value?.reply}")
+        }
+
+    }
+
+    override fun onError(t: Throwable?) {
+        println("[GRPC, Client] send version onError()")
+    }
+    override fun onCompleted() {
+        println("[GRPC, Client] send version onCompleted()")
+    }
+}
+
+class VersionResponseStreamObserver : StreamObserver<file_proto.ver_Response> {
+    override fun onNext(value: file_proto.ver_Response?) {
+        println("[GRPC, Client] send version = ${value?.version}")
+    }
+
+    override fun onError(t: Throwable?) {
+        println("[GRPC, Client] send version onError()")
+    }
+    override fun onCompleted() {
+        println("[GRPC, Client] send version onCompleted()")
+    }
+}
---
+++ src/main/kotlin/HelloServer.kt
@@ -0,0 +1,114 @@
+import com.google.protobuf.ByteString
+import com.google.protobuf.compiler.PluginProtos
+import io.grpc.stub.StreamObserver
+import com.main.file_proto.file_proto
+import com.main.file_proto.FileServiceGrpc
+import io.grpc.ServerBuilder
+import java.io.*
+import java.lang.Thread.sleep
+
+fun main() {
+    println("in main()")
+    val service = FileService()
+    val server = ServerBuilder
+        .forPort(10004) // ServerBuilder.forPort()에서  ProviderNotFoundException 발생, grpc-netty or grpc-netty-shaded를 종속성에 추가
+        .addService(FileService())
+        .build()
+
+    println("[GRPC] server starts()")
+    server.start()
+    server.awaitTermination()
+
+    var file = PluginProtos.CodeGeneratorResponse.File.newBuilder()
+}
+
+class FileService : FileServiceGrpc.FileServiceImplBase() {
+    override fun sayHello(request: file_proto.HelloRequest?, responseObserver: StreamObserver<file_proto.HelloResponse>?) {
+        println("sayHello(${request?.greeting})")
+
+        val response = file_proto.HelloResponse.newBuilder().setReply("hello, Client").build()
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
+    }
+
+    override fun fileDown(request: file_proto.HelloRequest?, responseObserver: StreamObserver<file_proto.file_Response>?) {
+        val BUF_SIZE = 4096;
+        var buffer = ByteArray(BUF_SIZE)
+        val input : InputStream = BufferedInputStream(FileInputStream("C:\\Server\\zlib-1.2.3.exe"));
+        println("[GRPC, Server] file down ...")
+        var nRead : Int  = input.read(buffer);
+        while (nRead > 0)
+        {
+            val resp = file_proto.file_Response.newBuilder()
+                .setFileBin(ByteString.copyFrom(buffer))
+                .setFileSize(nRead)
+                .build()
+            responseObserver?.onNext(resp)
+            sleep(50)
+            println("size is - ${nRead}")
+            nRead = input.read(buffer);
+        }
+        input.close();
+        responseObserver?.onCompleted()
+    }
+
+    override fun reqRes(responseObserver: StreamObserver<file_proto.HelloResponse>?): StreamObserver<file_proto.HelloRequest> {
+        return object : StreamObserver<file_proto.HelloRequest> {
+            override fun onNext(value: file_proto.HelloRequest?) {
+                //println("[GRPC, Server] bidiHello() - onNext(${value?.greeting})")
+                for(i in 0 until 3) {
+                    val resp = file_proto.HelloResponse.newBuilder()
+                        .setReply("\n${i}th Response to Client - (${value?.greeting})")
+                        .build()
+                    responseObserver?.onNext(resp)
+
+                    Thread.sleep(100)
+                }
+            }
+            override fun onError(t: Throwable?) {
+                println("[GRPC, Server] bidiHello() - onError()")
+            }
+
+            override fun onCompleted() {
+                println("[GRPC, Server] bidiHello() - onCompleted()")
+                responseObserver?.onCompleted()
+            }
+        }
+    }
+
+    override fun serverVersionInfo(request: file_proto.ver_Request?, responseObserver: StreamObserver<file_proto.ver_Response>?) {
+        for(i in 0 until 5) {
+            val resp = file_proto.ver_Response.newBuilder()
+                .setVersion("vi.0.0.$i")
+                .setFileInfo("version explain - $i")
+                .setFileInfo("hash - ${i*3}")
+                .build()
+            responseObserver?.onNext(resp)
+
+            Thread.sleep(1000)
+        }
+        responseObserver?.onCompleted()
+    }
+
+    override fun clientVersionInfo(responseObserver: StreamObserver<file_proto.ver_Response>?): StreamObserver<file_proto.ver_Request> {
+        return object : StreamObserver<file_proto.ver_Request> {
+
+            override fun onNext(value: file_proto.ver_Request?) {
+                println("[GRPC, Server] Client version is ${value?.version}\n")
+                println("[GRPC, Server] Client fileInfo is ${value?.fileInfo}\n")
+            }
+
+            override fun onError(t: Throwable?) {
+                println("[GRPC, Server] lotsOfGreetings() - onError()")
+            }
+
+            override fun onCompleted() {
+                println("[GRPC, Server] lotsOfGreetings() - onCompleted()")
+
+                val response = file_proto.ver_Response.newBuilder().setVersion("v1.0.0.3").build()
+                responseObserver?.onNext(response)
+                responseObserver?.onCompleted()
+            }
+        }
+    }
+}
